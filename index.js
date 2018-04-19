ac8e22822f8e4345d503bf417a09340677a922fd
Chris Henry <cdhenry@gmail.com>
2018-04-19 20:24:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickup){
+  return Math.abs(pickup - 42)
+}
+
+function distanceFromHqInFeet(pickup) {
+  return distanceFromHqInBlocks(pickup) * 264
+}
+
+function distanceTravelledInFeet(start, destination){
+  return Math.abs(destination - start) * 264
+}
+
+function calculatesFarePrice(start, destination) {
+  price = (distanceTravelledInFeet(start, destination) - 400) * .02
+  switch (price){
+    case 
+  }
+}
